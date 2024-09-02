26819ce8b569e8a186dc101e119da526e5810ec6
Mihail Pogosyan <mihail.pogosyan96@gmail.com>
2024-09-02 22:22:59 +0400

    add pwa support

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,31 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
-import { templateCompilerOptions } from '@tresjs/core'
+import { VitePWA } from 'vite-plugin-pwa';
 import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue({
-    ...templateCompilerOptions
+  plugins: [vue(), VitePWA({
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'AA Simulator 3.0',
+      short_name: 'AA Sim 3.0',
+      description: 'AA Simulator',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
   })],
   resolve: {
     alias: {
