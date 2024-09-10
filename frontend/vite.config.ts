6a7c4f012dd30b3aec9a28e3f1dcc8f6c4dbaf27
Mihail Pogosyan <mihail.pogosyan96@gmail.com>
2024-09-10 12:31:46 +0400

    add devtools

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
+import vueDevTools from 'vite-plugin-vue-devtools'
 import { VitePWA } from 'vite-plugin-pwa';
 import { templateCompilerOptions } from '@tresjs/core'
 import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue({ ...templateCompilerOptions }), VitePWA({
+  plugins: [vue({ ...templateCompilerOptions }),  vueDevTools(), VitePWA({
     registerType: 'autoUpdate',
     manifest: {
       name: 'AA Simulator 3.0',
