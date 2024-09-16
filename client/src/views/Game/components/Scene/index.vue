e16c9ab57b0a9322ff112fbebd2f6f2a8ceed71e
Mihail Pogosyan <mihail.pogosyan96@gmail.com>
2024-09-16 21:28:33 +0400

    improve graphic

--- client/src/views/Game/components/Scene/index.vue
+++ client/src/views/Game/components/Scene/index.vue
@@ -1,18 +1,19 @@
 <template>
     <Suspense>
-            <LoadIndicator />
-        </Suspense>
-    <TresCanvas window-size antialias alpha preset="realistic">
-        
-        <Sky :azimuth="0" :elevation="sunElevation" :turbidity="10" :mie-coefficient="0.005" :mie-directional-g="0"
-            :rayleigh="2" />
+        <LoadIndicator />
+    </Suspense>
+    <TresCanvas window-size antialias alpha preset="realistic" shadows>
+
+        <Sky :azimuth="0" :elevation="sunElevation"  />
+        <TresFog :far="3000" :color="0x404040"/>
 
         <!-- Освещение -->
-        <TresAmbientLight :color="0x404040" :intensity="0.8" />
+        <TresAmbientLight :color="0x404040" :intensity="0.8" :position="[0, Math.sin(sunElevationRad) * 100, Math.cos(sunElevationRad) * 100]" />
 
         <!-- DirectionalLight с синхронизацией с углом солнца -->
         <TresDirectionalLight :color="0xffffff" :intensity="1.2"
-            ::position="[0, Math.sin(sunElevationRad) * 100, Math.cos(sunElevationRad) * 100]" :look-at="[0, 0, 0]" />
+            :position="[0, Math.sin(sunElevationRad) * 100, Math.cos(sunElevationRad) * 100]" :look-at="[0, 0, 0]"
+            cast-shadow :shadow-mapSize-width="1024" :shadow-mapSize-height="1024" />
 
         <!-- Камера -->
         <TresPerspectiveCamera v-if="currentAA" :fov="75" :far="10000"
@@ -27,13 +28,13 @@
         <!-- Летающие обьекты -->
         <FlightObject v-for="flightObject in parsedFlightObjects" :flight-object="flightObject"
             :key="flightObject.id" />
-        <SmokeEmitter :flight-objects="parsedFlightObjects"/>
+        <SmokeEmitter :flight-objects="parsedFlightObjects" />
         <!-- Зенитки-->
         <AAObject v-for="aaObject in aas" :aaObject="aaObject" />
 
         <!-- Террейн -->
         <Suspense v-if="map">
-            <GLTFModel :path="mapPath" cast-shadow receive-shadow />
+            <GLTFModel :path="mapPath" receive-shadow/>
         </Suspense>
     </TresCanvas>
 </template>
@@ -41,7 +42,7 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia';
 import { TresCanvas } from '@tresjs/core'
-import { Sky, GLTFModel } from '@tresjs/cientos'
+import { Sky, GLTFModel, Box } from '@tresjs/cientos'
 import { useGameStore } from '../../../../stores/game';
 import { useDevice } from '../../../../stores/device';
 import FlightObject from './FlightObject.vue';
@@ -57,7 +58,7 @@ const deviceStore = useDevice()
 const { currentAA, parsedFlightObjects, aas, map, direction } = storeToRefs(gameStore);
 
 
-const sunElevation = 1;
+const sunElevation = 25;
 const sunElevationRad = sunElevation * (Math.PI / 180);
 
 
