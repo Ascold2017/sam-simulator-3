598a8ffb0e0fc544db5b1591c1a9faf27ec2dbec
Mihail Pogosyan <mihail.pogosyan96@gmail.com>
2024-09-16 14:31:58 +0400

    remove axes helper

--- client/src/views/MainScreen/components/Scene/index.vue
+++ client/src/views/MainScreen/components/Scene/index.vue
@@ -7,9 +7,6 @@
         <Sky :azimuth="0" :elevation="sunElevation" :turbidity="10" :mie-coefficient="0.005" :mie-directional-g="0"
             :rayleigh="2" />
 
-
-        <!-- The X axis is red. The Y axis is green. The Z axis is blue. -->
-        <TresAxesHelper :args="[100]" :position="[0, 100, 0]" />
         <!-- Освещение -->
         <TresAmbientLight :color="0x404040" :intensity="0.8" />
 
@@ -26,9 +23,6 @@
             @update-direction="direction = $event" @update-orientation="deviceStore.orientation = $event" />
         <CustomFirstPersonControl v-else :min-elevation="0" :max-elevation="Math.PI / 4" :look-speed="0.06"
             @update-direction="direction = $event" />
-
-
-        
 
         <!-- Летающие обьекты -->
         <FlightObject v-for="flightObject in parsedFlightObjects" :flight-object="flightObject"
