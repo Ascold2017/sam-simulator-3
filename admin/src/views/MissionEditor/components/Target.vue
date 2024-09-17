e846d3aca228d8b7a58104650c419af8d2de7c91
Mihail Pogosyan <mihail.pogosyan96@gmail.com>
2024-09-17 18:06:53 +0400

    refactoring

--- admin/src/views/MissionEditor/components/Target.vue
+++ admin/src/views/MissionEditor/components/Target.vue
@@ -1,22 +1,17 @@
 <template>
     <TresGroup>
-        <Line2 v-if="linePoints.length > 0"
-            :points="linePoints"
-            :line-width="2" color="white"
-            :key="linePointsKey"  />
+        <Line2 v-if="linePoints.length > 0" :points="linePoints" :line-width="2" color="white" :key="linePointsKey" />
         <Sphere v-for="(waypoint, index) in waypoints" :args="[30, 10, 10]"
             :position="[waypoint.position.x, waypoint.position.y, waypoint.position.z]"
             :color="getSphereColor(index, target.waypoints.length)"
             :user-data="{ isWaypoint: true, waypoint: waypoint, waypointIndex: index, target: target }"
             @click="emit('click', $event.object)" />
     </TresGroup>
-
-
 </template>
 
 <script setup lang="ts">
 import { Object3D } from 'three';
-import { Line2, Sphere } from '@tresjs/cientos'
+import { Line2, Sphere, Text3D } from '@tresjs/cientos'
 import { EditableTarget } from '../../../stores/missionEditor/targets';
 import { computed, watch, ref, watchEffect } from 'vue';
 
