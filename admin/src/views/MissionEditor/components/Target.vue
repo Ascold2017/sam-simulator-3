23123ea713bf1c2a8ea8f1747aff28cf2adbd0ed
Mihail Pogosyan <mihail.pogosyan96@gmail.com>
2024-09-17 17:52:05 +0400

    fix display waypoints

--- admin/src/views/MissionEditor/components/Target.vue
+++ admin/src/views/MissionEditor/components/Target.vue
@@ -1,8 +1,9 @@
 <template>
     <TresGroup>
-        <Line2 v-if="waypoints.length > 0"
-            :points="waypoints.map((waypoint) => [waypoint.position.x, waypoint.position.y, waypoint.position.z])"
-            :line-width="2" color="white" />
+        <Line2 v-if="linePoints.length > 0"
+            :points="linePoints"
+            :line-width="2" color="white"
+            :key="linePointsKey"  />
         <Sphere v-for="(waypoint, index) in waypoints" :args="[30, 10, 10]"
             :position="[waypoint.position.x, waypoint.position.y, waypoint.position.z]"
             :color="getSphereColor(index, target.waypoints.length)"
@@ -17,14 +18,17 @@
 import { Object3D } from 'three';
 import { Line2, Sphere } from '@tresjs/cientos'
 import { EditableTarget } from '../../../stores/missionEditor/targets';
-import { computed } from 'vue';
+import { computed, watch, ref, watchEffect } from 'vue';
 
 
 const props = defineProps<{ target: EditableTarget }>()
 
 const emit = defineEmits<{ click: [object: Object3D] }>()
 
-const waypoints = computed(() => props.target.waypoints)
+
+const waypoints = computed(() => props.target.waypoints);
+const linePoints = computed(() => waypoints.value.map((waypoint) => [waypoint.position.x, waypoint.position.y, waypoint.position.z]));
+const linePointsKey = computed(() => linePoints.value.map(p => p.join(',')).join('-'));
 
 const getSphereColor = (index: number, totalWaypoints: number) => {
     if (index === 0) return 'blue'; // Первый шар - синий
