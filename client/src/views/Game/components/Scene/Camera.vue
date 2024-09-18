a40dceb5f620036786b0253aba23a9400adf8eba
Mihail Pogosyan <mihail.pogosyan96@gmail.com>
2024-09-18 20:33:58 +0400

    fix camera

--- client/src/views/Game/components/Scene/Camera.vue
+++ client/src/views/Game/components/Scene/Camera.vue
@@ -1,7 +1,6 @@
 <template>
     <TresPerspectiveCamera v-if="currentAA" :fov="75" :far="10000" :zoom="zoom"
         :position="[currentAA.position.x, currentAA.position.y, currentAA.position.z]"
-        :look-at="[currentAA.position.x + 1, currentAA.position.y, currentAA.position.z]"
         :key="zoom + currentAA.position.x + currentAA.position.y + currentAA.position.z" />
 </template>
 
@@ -14,16 +13,17 @@ import { useLoop } from '@tresjs/core';
 const gameStore = useGameStore()
 const { currentAA, direction, viewMode } = storeToRefs(gameStore);
 
-const { render } = useLoop()
+const { render, } = useLoop()
 
 const zoom = computed(() => {
     if (viewMode.value === 'capture') return 3
     return 1
 });
 
-render(({ renderer, scene, camera }) => {
-    camera?.rotation.set(direction.value.elevation, direction.value.azimuth, 0, 'YXZ');
-    renderer.render(scene, camera)
 
+render(({ renderer, scene, camera }) => {
+    camera.rotation.set(direction.value.elevation, direction.value.azimuth, 0, 'YXZ');
+    renderer.render(scene, camera);
 });
+
 </script>
