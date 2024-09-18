838e4cf9b6ffcc48bbf4104569b324beb74a084e
Mihail Pogosyan <mihail.pogosyan96@gmail.com>
2024-09-18 13:49:20 +0400

    fix display aa

--- client/src/views/Game/components/Scene/AAObject.vue
+++ client/src/views/Game/components/Scene/AAObject.vue
@@ -1,7 +1,7 @@
 <template>
     <TresMesh :position="[aaObject.position.x, aaObject.position.y, aaObject.position.z]">
-        <TresConeGeometry :radius="3" :height="8" />
-        <TresMeshStandardMaterial :color="0x00ff00"/>
+        <TresConeGeometry :args="[3, 8, 5, 5]" />
+        <TresMeshBasicMaterial :color="0x00ff00"/>
     </TresMesh>
 </template>
 
