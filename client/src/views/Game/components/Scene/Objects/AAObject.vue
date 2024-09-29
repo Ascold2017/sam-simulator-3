138a58a3e395d92b2b51e42b1dea2ff83e09c479
Mihail Pogosyan <mihail.pogosyan96@gmail.com>
2024-09-29 12:35:26 +0400

    fix ammo count

--- client/src/views/Game/components/Scene/Objects/AAObject.vue
+++ client/src/views/Game/components/Scene/Objects/AAObject.vue
@@ -1,7 +1,7 @@
 <template>
     <TresGroup :position="aaObject.position">
         <TresMesh :position="[0, -7.5, 0]">
-            <TresConeGeometry :args="[3, 15, 32, 32]" />
+            <TresConeGeometry :args="[3, 12, 32, 32]" />
             <TresMeshBasicMaterial :color="0x00ff00" />
         </TresMesh>
     </TresGroup>
